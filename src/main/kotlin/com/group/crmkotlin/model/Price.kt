c5143e2a6333d3eb03aeb485e371bddfce118cd1
manuarlin <manuel.arlin@zenika.com>
2018-11-01 17:40:37 +0100

    show implicit getter and setter

--- src/main/kotlin/com/group/crmkotlin/model/Price.kt
+++ src/main/kotlin/com/group/crmkotlin/model/Price.kt
@@ -2,9 +2,13 @@ package com.group.crmkotlin.model
 
 class Price {
 
-    var amount : Double?
-    set(value) {
-    }
+    var amount: Double?
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
 
     constructor(_amount: Double) {
         amount = _amount
